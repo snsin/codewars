7ead95a3e67bb5cfa34c2ab70499e8f91a1cd016
Sergey Sinitsyn <sin_s_n@mail.ru>
2016-01-24 01:53:35 +0300

    gcd

--- codewars/src/ru/codewars/trains8k/Main.java
+++ codewars/src/ru/codewars/trains8k/Main.java
@@ -1,8 +1,14 @@
 package ru.codewars.trains8k;
 
+import java.util.Random;
+
 public class Main {
+	public static final Random rnd = new Random();
 
 	public static void main(String[] args) {
+		for (int i = 100; i - 1 > 0; --i) {
+			System.out.println(rnd.nextInt(200), rnd.nextInt(200));
+		}
 		System.out.println((new BrokenSequence().findMissingNumber("")));
 	}
 
--- codewars/src/ru/codewars/trains8k/WeirdPrimeGen.java
+++ codewars/src/ru/codewars/trains8k/WeirdPrimeGen.java
@@ -2,13 +2,41 @@ package ru.codewars.trains8k;
 
 public class WeirdPrimeGen {
     public static long countOnes(long n) {
+    	return 0;
         // your code
     }
     public static long maxPn(long n) {
+    	return 0;
         // your code
     }
     public static int anOverAverage(long n) {
+    	return 0;
         // your code
+    }
+    
+    public static long grcodi(long n, long m){
+    	return gcd(n, m);
+    }
+    
+    private static long gcd(long n, long m) {
+    	long d = 1;
+
+    	while ( n != 0 || m != 0) {
+    		if ((m % 2 == 0) && (n % 2 == 0)) {
+    			d *= 2;
+    			m /= 2;
+    			n /= 2;
+    		} else if ((m % 2 == 0) && (n % 2 != 0)) {
+    			m /= 2;
+    		} else if ((m % 2 != 0) && (n % 2 == 0)) {
+    			n /= 2;
+    		} else if ((m % 2 != 0) && (n % 2 != 0) && (m >= n)) {
+    			m -= n;
+    		} else if ((m % 2 != 0) && (n % 2 != 0) && (m < n)) {
+    			n -= m;
+    		}
+    	}
+    	return d;
     }
 
 }
